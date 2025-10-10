9fcdb9066e4a62a79e4639e4266c9bcfdfc65b8a
Peace <peacess@163.com>
2025-10-10 17:17:44 +0800

    builded

--- doors_app/flutter/doors/android/app/src/main/kotlin/com/doors/app/MainActivity.kt
+++ doors_app/flutter/doors/android/app/src/main/kotlin/com/doors/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.doors
+package com.doors.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
 rootProject.name = "Doors"
 
 include(":doors_lib")
-include(":doors_lib").projectDir = file("doors_lib/kotlin")
