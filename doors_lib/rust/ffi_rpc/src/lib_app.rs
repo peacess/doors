40c83d4957fc25164336ad2fb918e27808eb48a8
Peace <peacess@163.com>
2025-10-10 18:23:46 +0800

    init and uninit once

--- doors_lib/rust/ffi_rpc/src/lib_app.rs
+++ doors_lib/rust/ffi_rpc/src/lib_app.rs
@@ -1,22 +1,35 @@
+use std::sync::Arc;
+
 use tokio::runtime::Handle;
 
 pub struct LibApp {
     handle: Handle,
     shutdown_sender: tokio::sync::broadcast::Sender<()>,
-    // shutdown_receiver: tokio::sync::broadcast::Receiver<()>,
+    running_handle: Arc<std::sync::atomic::AtomicBool>,
 }
 
 static LIB_APP: std::sync::OnceLock<LibApp> = std::sync::OnceLock::new();
 
 impl LibApp {
-    pub fn make_app() -> Result<Self, anyhow::Error> {
-        let rt = tokio::runtime::Builder::new_multi_thread().worker_threads(4).enable_all().build()?;
-        let handle = rt.handle().clone();
-        let (shutdown_sender, _shutdown_receiver) = tokio::sync::broadcast::channel::<()>(1);
+    fn make_app() -> Result<Self, anyhow::Error> {
+        let runtime = tokio::runtime::Builder::new_multi_thread().worker_threads(4).enable_all().build()?;
+        let (shutdown_sender, mut shutdown_receiver) = tokio::sync::broadcast::channel::<()>(1);
+        let handle = runtime.handle().clone();
+        let running_handle = Arc::new(std::sync::atomic::AtomicBool::new(true));
+        let running_handle_thread = running_handle.clone();
+        std::thread::spawn(move || {
+            let _en = runtime.enter();
+            runtime.block_on(async {
+                if let Err(e) = shutdown_receiver.recv().await {
+                    log::error!("{}", e);
+                }
+                running_handle_thread.store(false, std::sync::atomic::Ordering::Relaxed);
+            });
+        });
         Ok(Self {
             handle,
             shutdown_sender,
-            // shutdown_receiver,
+            running_handle,
         })
     }
 
@@ -30,6 +43,7 @@ impl LibApp {
                 }
             });
         });
+
         result.map_err(|e| {
             let message = {
                 if let Some(err_str) = e.downcast_ref::<&str>() {
@@ -42,5 +56,35 @@ impl LibApp {
             };
             anyhow::anyhow!("LibApp panicked: {}", message)
         })
+    }
+
+    pub fn uninit(&self) {
+        if self.running_handle.load(std::sync::atomic::Ordering::Relaxed) {
+            if let Err(e) = self.shutdown_sender.send(()) {
+                log::error!("{}", e);
+            }
+        }
+    }
+
+    pub fn handle(&self) -> Option<&Handle> {
+        if self.running_handle.load(std::sync::atomic::Ordering::Relaxed) {
+            Some(&self.handle)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lib_app::{LIB_APP, LibApp};
+
+    #[test]
+    fn it_works() {
+        LibApp::init().unwrap();
+        if let Some(app) = LIB_APP.get() {
+            app.handle.spawn(async {});
+            app.uninit();
+        }
     }
 }
