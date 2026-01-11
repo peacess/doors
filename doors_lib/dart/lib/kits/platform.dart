dd5f5841a7644368d95811a2a0c5fe4f7bde2b4b
Peace <peacess@163.com>
2026-01-11 10:21:31 +0800

    upgrade

--- doors_lib/dart/lib/kits/platform.dart
+++ doors_lib/dart/lib/kits/platform.dart
@@ -3,5 +3,5 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 abstract class PlatformEx {
-  static get isDesktop => !kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS);
+  static bool get isDesktop => !kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS);
 }
